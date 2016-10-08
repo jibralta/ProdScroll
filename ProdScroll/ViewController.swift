a83e6add1cb0cfebd7f69685ea3787d5fe0f6d2f
Joy Umali <joyumali@gmail.com>
2016-10-08 16:50:32 -0700

    added swipe gesture

--- ProdScroll/ViewController.swift
+++ ProdScroll/ViewController.swift
@@ -12,16 +12,72 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var scrollView: UIScrollView!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    var images = [UIImageView]()
+    
+    var maxPage = 9
+    var minPage = 0
+    var currentPage = 0
+    
+    override func viewDidAppear(_ animated: Bool) {
+        
+        var contentWidth: CGFloat = 0.0    //variable created to store content width to set content size.
+        
+        let scrollWidth = scrollView.frame.size.width
+        
+        for x in 0...9 {
+            
+            let image = UIImage(named: "\(x)_prod.jpg")     // picking from the assets
+            let imageView = UIImageView(image: image)   // and placing into ImageView
+            images.append(imageView)                     // adding image to the array above
+            
+            // Adding icons to the scroll view. Each will be in the middle but on different screens
+            var newX: CGFloat = 0.0 // origin or placement point is top left.
+            newX = scrollWidth / 2 + scrollWidth * CGFloat(x)
+            contentWidth += newX
+            scrollView.addSubview(imageView)    //adds the imageView icon to scrollView
+            
+            imageView.frame = CGRect(x: newX - 75, y: (scrollView.frame.size.height / 2) - 75, width: 150, height: 150)
+            
+            imageView.bringSubview(toFront: imageView)
+        }
+        
+        scrollView.clipsToBounds = false
+        
+        scrollView.contentSize = CGSize(width: contentWidth, height: view.frame.size.height)
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    
+    @IBAction func detectSwipe(_ sender: UISwipeGestureRecognizer) {
+        
+        if sender.direction == UISwipeGestureRecognizerDirection.right && currentPage <= maxPage {
+            moveScrollView(direction: 1)
+        }
+        
+        if sender.direction == UISwipeGestureRecognizerDirection.left && currentPage >= minPage {
+            moveScrollView(direction: -1)
+        }
     }
-
+    
+    func moveScrollView(direction:Int) {
+        currentPage += direction
+        let movement: CGPoint = CGPoint(x: scrollView.frame.size.width * CGFloat(currentPage), y: 0.0)
+        scrollView.setContentOffset(movement, animated: true)
+        
+//                // Create a animation to increase the actual icon on screen
+//                UIView.animate(withDuration: 0.4){
+//                    self.images[self.currentPage].transform = CGAffineTransform.init(scaleX: 1.4, y: 1.4)
+//        
+//                    // Revert icon size of the non-active pages
+//                    for x in 0..<self.images.count {
+//                        if (x != self.currentPage) {
+//                            self.images[x].transform = CGAffineTransform.identity
+//                        }
+//                    }
+//                }
+    }
+    
 
 }
 
